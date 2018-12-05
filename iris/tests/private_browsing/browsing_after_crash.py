b3a2ad61801ffc2eb9749dfafbd71e1dcf834bdb
Dmitry Bakaev <bakaev.dmitriy@gmail.com>
2018-12-05 18:00:13 +0200

    Fixed issue 1567 and changed method launch_firefox() and restart_firefox() to make Crash Reporting Tool available

--- iris/tests/private_browsing/browsing_after_crash.py
+++ iris/tests/private_browsing/browsing_after_crash.py
@@ -15,32 +15,23 @@ class Test(BaseTest):
         self.test_suite_id = '1826'
         self.locales = ['en-US']
 
-    def setup(self):
-        """Test case setup
-
-        This overrides the setup method in the BaseTest class, so that it can use a brand new profile.
-        """
-        BaseTest.setup(self)
-        self.profile = Profile.DEFAULT
-
-        return
-
     def run(self):
         soap_label_pattern = Pattern('soap_label.png')
         private_browsing_icon_pattern = Pattern('private_browsing_icon.png')
-        firefox_icon_dock_pattern = Pattern('firefox_logo_dock.png')
-
         preference_name_label_pattern = Pattern('preference_name_label.png')
         devtools_label_pattern = Pattern('devtools_regular_label.png')
         run_button_pattern = Pattern('run_button.png')
-        restore_session_button_pattern = Pattern('restore_session_button.png')
         restart_firefox_button_pattern = Pattern('restart_firefox_button.png')
         mozilla_crash_reporter_label_pattern = Pattern('mozilla_crash_reporter_label.png')
         soap_wikipedia_header_label_pattern = Pattern('soap_wikipedia_header_label.png')
         accept_the_risk_button_pattern = Pattern('accept_the_risk_button.png')
 
-        navigate(LocalWeb.FIREFOX_TEST_SITE)
-        exists(LocalWeb.FIREFOX_LOGO, 10)
+        restart_firefox(self,
+                        self.browser.path,
+                        self.profile_path,
+                        LocalWeb.FIREFOX_TEST_SITE,
+                        image=LocalWeb.FIREFOX_LOGO,
+                        show_crash_reporter=True)
 
         new_private_window()
         private_browsing_window_opened = exists(private_browsing_icon_pattern, 5)
@@ -78,14 +69,11 @@ class Test(BaseTest):
               'ctypes.cast(zero, ctypes.PointerType(ctypes.int32_t)); badptr.contents;')
         click(run_button_pattern)
 
-        """
         firefox_crashed = exists(restart_firefox_button_pattern, 10)
         assert_true(self, firefox_crashed, 'Firefox crashed.')
 
-        
         click(restart_firefox_button_pattern)
 
-        # wait
         mozilla_crash_reporter_label_exists = exists(mozilla_crash_reporter_label_pattern, 10)
         assert_true(self, mozilla_crash_reporter_label_exists, 'Crash report windows exists.')
 
@@ -95,27 +83,8 @@ class Test(BaseTest):
         except FindError:
             raise FindError('Crash report is not dismissed')
 
-        firefox_icon_dock_exists = exists(firefox_icon_dock_pattern, 5)
-        assert_true(self, firefox_icon_dock_exists, 'Firefox icon exists')
-        click(firefox_icon_dock_pattern)
-
-        restore_session_exists = exists(restore_session_button_pattern, 10)
+        restore_session_exists = exists(LocalWeb.FIREFOX_LOGO, 10)
         assert_true(self, restore_session_exists, 'Firefox restored')
-
-        click(restore_session_button_pattern)
-
-        preference_name_label_exists = exists(preference_name_label_pattern, 5)
-        assert_true(self, preference_name_label_exists, 'Session is restored.')
-        """
-
-        self.firefox_runner = launch_firefox(path=self.browser.path, profile=Profile.DEFAULT, url='about:sessionrestore',
-                                             )
-        self.firefox_runner.start()
-
-        restore_session_exists = exists(restore_session_button_pattern, 10)
-        assert_true(self, restore_session_exists, 'Firefox restored')
-
-        click(restore_session_button_pattern)
 
         wiki_label_exists = exists(soap_wikipedia_header_label_pattern, 2)
         assert_false(self, wiki_label_exists, 'The Private Browsing window is not restored.')
