1a512e162f48f65ce0ecec2b4799bd6de23b3dae
paulprokhorovmoz <pauprokhorov@mozilla.com>
2018-12-05 15:35:20 +0200

    Added linux specific changes

--- iris/tests/private_browsing/track_protection_turn_off_for_site.py
+++ iris/tests/private_browsing/track_protection_turn_off_for_site.py
@@ -34,52 +34,53 @@ class Test(BaseTest):
         # Access the following website.
         new_tab()
         navigate("https://edition.cnn.com")
-        repeat_key_down(8)
         website_displayed = exists(cnn_site_logo_pattern, 30)
         assert_true(self, website_displayed, 'The website is successfully displayed')
-        tracking_protection_shield_displayed = exists(tracking_protection_shield_pattern, 3)
+        restore_firefox_focus()
+        repeat_key_down(10)
+        tracking_protection_shield_displayed = exists(tracking_protection_shield_pattern, 5)
         assert_true(self, tracking_protection_shield_displayed, 'The tracking protection shield is displayed')
-        cnn_blocked_content_displayed = exists(cnn_blocked_content_pattern, 3)
+        cnn_blocked_content_displayed = exists(cnn_blocked_content_pattern, 30)
         assert_false(self, cnn_blocked_content_displayed,
                      'Websites content that contain tracking elements are not displayed on the page')
 
         # Click the tracking protection shield icon
         click(tracking_protection_shield_pattern)
-        disable_blocking_button_displayed = exists(disable_blocking_button_pattern, 3)
+        disable_blocking_button_displayed = exists(disable_blocking_button_pattern, 5)
         assert_true(self, disable_blocking_button_displayed, 'The site information panel is displayed')
 
         # Click the "Disable protection for this session" option
         click(disable_blocking_button_pattern)
         website_displayed = exists(cnn_site_logo_pattern, 30)
         assert_true(self, website_displayed, 'The website successfully refreshes')
-        tracking_protection_shield_deactivated_displayed = exists(tracking_protection_shield_deactivated_pattern, 3)
+        tracking_protection_shield_deactivated_displayed = exists(tracking_protection_shield_deactivated_pattern, 5)
         assert_true(self, tracking_protection_shield_deactivated_displayed,
                     'The tracking protection shield is displayed as deactivated')
         mouse_move(tracking_protection_shield_deactivated_pattern)
-        tracking_content_detected_message_displayed = exists(tracking_content_detected_message_pattern, 10)
+        tracking_content_detected_message_displayed = exists(tracking_content_detected_message_pattern, 5)
         assert_true(self, tracking_content_detected_message_displayed,
                     'The tracking protection shield displays a "Tracking content detected" tooltip message')
-        cnn_blocked_content_displayed = exists(cnn_blocked_content_pattern, 3)
+        cnn_blocked_content_displayed = exists(cnn_blocked_content_pattern, 30)
         assert_true(self, cnn_blocked_content_displayed,
                     'Websites content that contain tracking elements are displayed on the page')
 
         # Click the tracking protection shield icon
         click(tracking_protection_shield_deactivated_pattern)
-        enable_blocking_button_displayed = exists(enable_blocking_button_pattern, 3)
+        enable_blocking_button_displayed = exists(enable_blocking_button_pattern, 5)
         assert_true(self, enable_blocking_button_displayed, 'The site information panel is displayed')
 
         # Click the "Enable protection" option
         click(enable_blocking_button_pattern)
         website_displayed = exists(cnn_site_logo_pattern, 30)
         assert_true(self, website_displayed, 'The website successfully refreshes')
-        tracking_protection_shield_displayed = exists(tracking_protection_shield_pattern, 3)
+        tracking_protection_shield_displayed = exists(tracking_protection_shield_pattern, 5)
         assert_true(self, tracking_protection_shield_displayed,
                     'The tracking protection shield is displayed as activated')
         mouse_move(tracking_protection_shield_pattern)
-        tracking_attempts_blocked_message_displayed = exists(tracking_attempts_blocked_message_pattern, 10)
+        tracking_attempts_blocked_message_displayed = exists(tracking_attempts_blocked_message_pattern, 5)
         assert_true(self, tracking_attempts_blocked_message_displayed,
                     'The tracking protection shield displays a "Tracking attempts blocked" tooltip message')
-        cnn_blocked_content_displayed = exists(cnn_blocked_content_pattern, 3)
+        cnn_blocked_content_displayed = exists(cnn_blocked_content_pattern, 30)
         assert_false(self, cnn_blocked_content_displayed,
                      'Websites content that contain tracking elements are not displayed on the page')
         close_window()
