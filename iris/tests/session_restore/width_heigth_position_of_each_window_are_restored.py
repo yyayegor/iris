e3df5ab4a34d79322de8939e25004fbdbc61ca44
Egor Yartsev <yartsev@NIXSOLUTIONS.COM>
2018-12-13 16:33:10 +0200

    Session restored, tabs' positions matched

--- iris/tests/session_restore/width_heigth_position_of_each_window_are_restored.py
+++ iris/tests/session_restore/width_heigth_position_of_each_window_are_restored.py
@@ -4,99 +4,113 @@ from iris.test_case import *
 class Test(BaseTest):
 
     def __init__(self):
-
         BaseTest.__init__(self)
         self.meta = "Width, height and position of each window are restored."
+        self.window_size = None
         self.test_case_id = '114826'
         self.test_suite_id = '1588'
         self.locales = ['en-US']
 
     def run(self):
-        """
-        Open new tabs;
-        drag the first opened tab from window;
-        drag new window lower;
-        drag the second opened tab from basic window;
-        drag new window aside;
-        open hamburger menu;
-        click "Quit"/"Exit";
-        click "Close Tabs";
-        restart firefox;
-        restore session;
-        check windows;
-        """
+
         firefox_test_site_tab_pattern = Pattern("firefox_test_site_tab.png")
         focus_test_site_tab_pattern = Pattern("focus_test_site_tab.png")
         iris_pattern = Pattern("iris_tab.png")
         hamburger_menu_button_pattern = Pattern("hamburger_menu_button.png")
         hamburger_menu_quit_item_pattern = Pattern("hamburger_menu_quit_item.png")
-        close_all_tabs_button_pattern = Pattern("close_all_tabs_button.png")
+        restore_previous_session_pattern = Pattern("restore_previous_session_item.png")
 
-
-        time.sleep(3)
-
+        change_preference("devtools.chrome.enabled", True)
 
         new_tab()
         navigate(LocalWeb.FIREFOX_TEST_SITE)
-        tab_1_loaded = exists(firefox_test_site_tab_pattern, 10)
-        assert_true(self, tab_1_loaded, "First tab loaded")
-        tab_1_location = image_search(firefox_test_site_tab_pattern)
+
+        tab_one_loaded = exists(firefox_test_site_tab_pattern, 10)
+        assert_true(self, tab_one_loaded, "First tab loaded")
 
         new_tab()
         navigate(LocalWeb.FOCUS_TEST_SITE)
-        tab_2_loaded = exists(focus_test_site_tab_pattern, timeout=1)
-        assert_true(self, tab_2_loaded, "Second tab loaded")
 
-        # new_tab()
-        # new_tab_location = image_search(new_tab_pattern)
-        # window_top_region = Region(x=0, y=(new_tab_location.y - SCREEN_HEIGHT/20),
-        #                            width=SCREEN_WIDTH, height=SCREEN_HEIGHT/10)
-        # hamburger_menu_button_location = image_search(hamburger_menu_button_pattern, window_top_region)
+        tab_two_loaded = exists(focus_test_site_tab_pattern, timeout=1)
+        assert_true(self, tab_two_loaded, "Second tab loaded")
 
-        tab_1_drop_location = Location(x=tab_1_location.x,
-                                       y=(tab_1_location.y + SCREEN_HEIGHT/10))
-
-        drag_drop(tab_1_location, tab_1_drop_location, duration=0.5)
         minimize_window()
-        time.sleep(1)
-        tab_1_new_location = image_search(firefox_test_site_tab_pattern)
-        tab_1_drop_location.left(tab_1_location.x)
-        drag_drop(tab_1_new_location, tab_1_drop_location, duration=0.5)
-        time.sleep(1)
 
-        tab_2_location = image_search(focus_test_site_tab_pattern)
+        tab_two_location = image_search(focus_test_site_tab_pattern)
 
-        tab_2_drop_location = Location(x=(tab_2_location.x + SCREEN_WIDTH / 5),
-                                       y=(tab_2_location.y + SCREEN_HEIGHT / 5))
+        tab_two_drop_location = Location(x=0,
+                                         y=(tab_two_location.y + SCREEN_HEIGHT / 5))
 
-        drag_drop(tab_2_location, tab_2_drop_location)
-        minimize_window()
-        time.sleep(1)
-        tab_2_new_location = image_search(focus_test_site_tab_pattern)
-        drag_drop(tab_2_new_location, tab_2_drop_location)
-        # hover(Location(SCREEN_WIDTH, SCREEN_HEIGHT), duration=1)
+        drag_drop(tab_two_location, tab_two_drop_location)
+
+        exists(focus_test_site_tab_pattern)
+        tab_two_new_location = image_search(focus_test_site_tab_pattern)
+
+        tab_one_location = image_search(firefox_test_site_tab_pattern)
+
+        tab_one_drop_location = Location(x=(tab_one_location.x + SCREEN_WIDTH / 5),
+                                         y=(tab_one_location.y + SCREEN_HEIGHT / 10))
+
+        drag_drop(tab_one_location, tab_one_drop_location, duration=0.5)
+        tab_one_window_region = Region(tab_one_drop_location.x,
+                                       tab_one_drop_location.y,
+                                       width=SCREEN_WIDTH,
+                                       height=SCREEN_HEIGHT / 5)
+        exists(firefox_test_site_tab_pattern)
+        tab_one_new_location = image_search(firefox_test_site_tab_pattern)
+        tab_one_drop_location.left(tab_one_location.x)
+        exists(iris_pattern)
         first_window_tab = image_search(iris_pattern)
-        click(first_window_tab)
-        minimize_window()
-        click(tab_2_drop_location)
-        hamburger_menu = image_search(hamburger_menu_button_pattern)
+        hamburger_menu = image_search(hamburger_menu_button_pattern, region=tab_one_window_region)
         click(hamburger_menu)
-        exit_item = image_search(hamburger_menu_quit_item_pattern)
-        click(exit_item, )
-        time.sleep(1)
-        close_all_tabs_button = image_search(close_all_tabs_button_pattern)
-        click(close_all_tabs_button, duration=1.5)
-        self.window_size = "200x300"
+        exists(hamburger_menu_quit_item_pattern, )
+        exit_item = find(hamburger_menu_quit_item_pattern, )
+        click(exit_item, duration=1)
+
+        args = ['-width 400', '-height 400']
         restart_firefox(self,
                         self.browser.path,
                         self.profile_path,
-                        self.base_local_web_url)
-        # minimize_window()
+                        self.base_local_web_url,
+                        args=args)
+        exists(hamburger_menu_button_pattern)
+        hamburger_menu_new_window = find(hamburger_menu_button_pattern)
+        click(hamburger_menu_new_window)
+        exists(restore_previous_session_pattern)
+        restore_previous_session = find(restore_previous_session_pattern)
 
-        # click(hamburger_menu_button_location)
-        # time.sleep(0.5)
-        # click(image_search(hamburger_menu_quit_item_pattern))
-        # time.sleep(1)
-        # click(image_search(close_all_tabs_button_pattern))
-        # restart_firefox(self, )
+        click(restore_previous_session)
+        exists(focus_test_site_tab_pattern)
 
+        firefox_test_site_restored_position = image_search(firefox_test_site_tab_pattern)
+        firefox_test_site_restored_coordinates = (firefox_test_site_restored_position.x,
+                                                  firefox_test_site_restored_position.y)
+        firefox_test_site_old_coordinates = (tab_one_new_location.x,
+                                             tab_one_new_location.y)
+        assert_equal(self,
+                     firefox_test_site_restored_coordinates,
+                     firefox_test_site_old_coordinates,
+                     "First tab position matched")
+
+        focus_site_restored_position = image_search(focus_test_site_tab_pattern)
+        focus_site_restored_coordinates = (focus_site_restored_position.x,
+                                           focus_site_restored_position.y)
+        focus_site_old_coordinates = (tab_two_new_location.x,
+                                      tab_two_new_location.y)
+        assert_equal(self,
+                     focus_site_restored_coordinates,
+                     focus_site_old_coordinates,
+                     "Second tab position matched")
+
+        iris_tab_restored_position = image_search(iris_pattern)
+        iris_tab_restored_coordinates = (iris_tab_restored_position.x,
+                                         iris_tab_restored_position.y)
+        iris_tab_old_coordinates = (first_window_tab.x,
+                                    first_window_tab.y)
+        assert_equal(self,
+                     iris_tab_restored_coordinates,
+                     iris_tab_old_coordinates,
+                     "Default iris tab position matched")
+
+        close_window()
+        close_window()
